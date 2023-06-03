b51b274507a61108d6a08e1e75806aaf78b89990
nikhilparmar <nkumarg@stevens.edu>
2023-06-03 19:44:30 -0400

    Arrays

---
+++ 13.Arrays.js
@@ -0,0 +1,8 @@
+// Creating an Array
+let arraylength = 5;
+let arr1 = []; // one way with square brackets.
+let arr2 = Array(arraylength); // another way with Array object.
+console.log(arr1.length); //checking length
+console.log(arr2.length);
+
+
