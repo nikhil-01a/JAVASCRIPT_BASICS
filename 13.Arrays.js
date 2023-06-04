0dbce1584e5f8a93a2059a9e7fa37d76016c9700
nikhilparmar <nkumarg@stevens.edu>
2023-06-04 14:49:40 -0400

    Populating arrays

--- 13.Arrays.js
+++ 13.Arrays.js
@@ -5,4 +5,11 @@ let arr2 = Array(arraylength); // another way with Array object.
 console.log(arr1.length); //checking length
 console.log(arr2.length);
 
-
+//Populating array
+let arr3 = ["A",true,2];
+console.log(arr3[0]);
+console.log[arr3[1]];
+console.log(arr3[arr3.length-1]);
+arr2[0]="sample at 0 of aar2";
+console.log(arr2[0]);
+console.log(arr2[1]);
